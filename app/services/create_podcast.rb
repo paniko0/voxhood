c08acae5062234ba024700f11e2fa4c391a8e58a
Danillo Souza <danillo.fs@gmail.com>
2016-08-12 20:17:35 -0600

    avoid variable shadowing

--- app/services/create_podcast.rb
+++ app/services/create_podcast.rb
@@ -1,10 +1,10 @@
 class CreatePodcast
   def self.create(params)
-    podcast = Podcast.where(itunes_id: params[:collectionId]).first_or_create do |podcast|
-      podcast.name = params[:artistName]
-      podcast.track_name = params[:trackName]
-      podcast.feed_url = params[:feedUrl]
-      podcast.art = params[:artworkUrl100]
+    podcast = Podcast.where(itunes_id: params[:collectionId]).first_or_create do |new_podcast|
+      new_podcast.name = params[:artistName]
+      new_podcast.track_name = params[:trackName]
+      new_podcast.feed_url = params[:feedUrl]
+      new_podcast.art = params[:artworkUrl100]
     end
 
     GetEpisodesJob.perform_later podcast
